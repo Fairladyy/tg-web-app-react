346cd9df5851dbe1c39562bdddd524fc0450d8d3
Fairladyy <jegor.sinn33@gmail.com>
2022-12-01 19:31:26 +0200

    o boje

--- src/components/ProductList/ProductList.jsx
+++ src/components/ProductList/ProductList.jsx
@@ -1,10 +1,107 @@
-import React from 'react'
-import './ProductList.css'
+import React from "react";
+import { useTelegram } from "../../Hooks/useTelegram";
+import ProductItem from "../ProductItem/ProductItem";
+import "./ProductList.css";
 
-function ProductList() {
+const products = [
+  {
+    id: "1",
+    title: "Джинсы",
+    price: 5000,
+    description: "Синего цвета, прямые",
+  },
+  {
+    id: "2",
+    title: "Куртка",
+    price: 12000,
+    description: "Зеленого цвета, теплая",
+  },
+  {
+    id: "3",
+    title: "Худи",
+    price: 7000,
+    description: "Красного цвета, из овечей шкуры",
+  },
+  {
+    id: "4",
+    title: "Рубашка",
+    price: 3000,
+    description: "Белого цвета, идеально для свадьбы",
+  },
+  {
+    id: "5",
+    title: "Носки",
+    price: 500,
+    description: "Чёрного цвета, вонять начнут через 3 дня, отвечаю",
+  },
+  {
+    id: "6",
+    title: "Трусы",
+    price: 800,
+    description: "Розового цвета, с сердечками<3",
+  },
+  {
+    id: "7",
+    title: "Шапка",
+    price: 2000,
+    description: "Чёрного цвета, мудацкая шапка",
+  },
+  {
+    id: "8",
+    title: "Кастрюля",
+    price: 5000,
+    description: "Чугунная, для плова - самое то",
+  },
+  {
+    id: "9",
+    title: "Замок для кастрюли",
+    price: 300,
+    description: "Стальной, кодовый, шоб цыгане в германии не спиздили",
+  },
+];
+
+const getTotalPrice = (items = []) => {
+  return items.reduce((acc, item) => {
+    return (acc += item.price);
+  }, 0);
+};
+
+const ProductList = () => {
+  const [addedItems, setAddedItems] = useState([]);
+  const { tg } = useTelegram();
+
+  const onAdd = (product) => {
+    const alreadyAdded = addedItems.find((item) => item.id === product.id);
+    let newItems = [];
+
+    if (alreadyAdded) {
+      newItems = addedItems.filter((item) => item.id !== product.id);
+    } else {
+      newItems = [...addedItems, product];
+    }
+
+    setAddedItems(newItems);
+
+    if (newItems.length === 0) {
+      tg.MainButton.hide();
+    } else {
+      tg.MainButton.show();
+      tg.MainButton.setParams({
+        text: `Купить ${getTotalPrice(newItems)}`,
+      });
+    }
+  };
   return (
-    <div>ProductList</div>
-  )
-}
+    <div className={"list"}>
+      {products.map((item) => (
+        <ProductItem>
+          product={item}
+          onAdd={onAdd}
+          className={"item"}
+        </ProductItem>
+      ))}
+    </div>
+  );
+};
 
-export default ProductList
+export default ProductList;
